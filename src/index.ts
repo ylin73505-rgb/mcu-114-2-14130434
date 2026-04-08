0a976b5baf1fa064d0594bb213107c4d415e087d
Oliver <ylin73505@gmail.com>
2026-04-08 17:18:17 +0800

    feat(ts): 列出會顯示的產品清單資料

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { Product } from "./product";
 let products = [
   new Product(1, "圖像 Angular 開發入門 第二版", "博碩文化", 500, true, new Date("2024-4-10")),
   new Product(2, "金魚都能懂的 CSS 必學屬性", "博碩文化", 500, true, new Date("2024-4-10")),
+  new Product(3, "書籍 A", "博碩文化", 500, false, new Date("2024-4-10")),
+  new Product(4, "書籍 B", "博碩文化", 500, true, new Date("2024-4-10")),
+  new Product(5, "書籍 C", "博碩文化", 500, false, new Date("2024-4-10")),
+  new Product(6, "書籍 D", "博碩文化", 500, false, new Date("2024-4-10")),
 ];
 
-console.log(products);
-
-products[0]!.setDisplay(false);
-
-console.table(products);
-
-function setProductDisplay(product: Product, isShow: boolean) {
-  product.isShow = isShow;
+for (const item of products) {
+  if (item.isShow) {
+    console.table(item);
+  }
 }
