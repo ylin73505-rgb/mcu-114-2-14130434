c0cf4c8075423842bc8a1b45c7cee16301d5b8ba
Oliver <ylin73505@gmail.com>
2026-04-08 17:54:47 +0800

    feat(ts): 新增與刪除產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Product } from "./product";
 
-let products = [
+let products: Product[] = [
   new Product(1, "圖像 Angular 開發入門 第二版", "博碩文化", 500, true, new Date("2024-4-10")),
   new Product(2, "金魚都能懂的 CSS 必學屬性", "博碩文化", 500, true, new Date("2024-4-10")),
   new Product(3, "書籍 A", "博碩文化", 500, false, new Date("2024-4-10")),
@@ -9,6 +9,40 @@ let products = [
   new Product(6, "書籍 D", "博碩文化", 500, false, new Date("2024-4-10")),
 ];
 
-const displayProducts = products.filter((item) => item.isShow);
+const newProduct = new Product(0, "書籍 E", "博碩文化", 500, false, new Date("2024-4-10"));
+addProduct(newProduct);
+console.table(products);
 
-displayProducts.forEach((item) => console.table(item));
+removeProduct(6);
+console.table(products);
+
+removeProductById(4);
+console.table(products);
+
+// 新增產品
+function addProduct(product: Product): void {
+  const id = products.length === 0 ? 1 : Math.max(...products.map(({ id }) => id)) + 1;
+  product.id = id;
+  products.push(product);
+}
+
+// 刪除產品 By index
+function removeProduct(index: number): void {
+  products.splice(index, 1);
+}
+
+// 刪除產品 By id
+function removeProductById(removeId: number): void {
+  const index = products.findIndex(({ id }) => id === removeId);
+  products.splice(index, 1);
+}
+
+/*
+products.push(newProduct);
+console.table(products);
+
+const removeId = 6;
+const index = products.findIndex(({ id }) => id === removeId);
+products.splice(index, 1);
+console.table(products);
+*/
