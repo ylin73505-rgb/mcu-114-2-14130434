c07662f49e6da38f5f36d53a2d723925979076a8
Oliver <ylin73505@gmail.com>
2026-04-08 17:09:04 +0800

    feat(ts): 利用函式設定產品是否顯示

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,13 @@ let products = [
   new Product(2, "金魚都能懂的 CSS 必學屬性", "博碩文化", 500, true, new Date("2024-4-10")),
 ];
 
-console.log(products);
 console.table(products);
+
+const product = products[0];
+setProductDisplay(products[0]!, false);
+
+console.table(products);
+
+function setProductDisplay(product: Product, isShow: boolean) {
+  product.isShow = isShow;
+}
