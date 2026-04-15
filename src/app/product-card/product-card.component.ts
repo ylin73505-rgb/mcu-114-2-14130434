1e25db714aff455fd4f3c5f8da2456b38fbfa433
Oliver <ylin73505@gmail.com>
2026-04-15 18:16:05 +0800

    feat(product): 利用事件繫結設定產品是否顯示

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -10,4 +10,10 @@ export class ProductCardComponent {
   protected productName = '書籍 A';
   protected author = '作者甲、作者乙、作者丙';
   protected company = '博碩文化';
+
+  protected isShow = true;
+
+  protected onSetDisplay(isShow: boolean): void {
+    this.isShow = isShow;
+  }
 }
