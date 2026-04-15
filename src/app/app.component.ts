fad329ba3bac59bbf0dcb46611d0784c396664eb
Oliver <ylin73505@gmail.com>
2026-04-15 18:05:07 +0800

    feat(product): 產品卡片樣式實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
