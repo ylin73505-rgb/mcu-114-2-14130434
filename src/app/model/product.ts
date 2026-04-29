3181cddad2ea7c058e642436aff703213f3c6f5d
Oliver <ylin73505@gmail.com>
2026-04-29 17:15:41 +0800

    feat(product): 建立產品資料模型

---
+++ src/app/model/product.ts
@@ -0,0 +1,22 @@
+export class Product {
+  constructor(initData?: Partial<Product>) {
+    if (!initData) return;
+    Object.assign(this, initData);
+  }
+
+  id!: number;
+
+  name!: string;
+
+  company!: string;
+
+  authors!: string;
+
+  price!: number;
+
+  isShow!: boolean;
+
+  photoUrl!: string;
+
+  createDate!: Date;
+}
--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-card-list',
@@ -8,15 +9,18 @@ import { ProductCardComponent } from '../product-card/product-card.component';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  protected productName = '書籍 A';
-  protected author = '作者甲、作者乙、作者丙';
-  protected company = '博碩文化';
+  protected product = new Product({
+    id: 1,
+    name: '書籍 A',
+    authors: '作者甲、作者乙、作者丙',
+    company: '博碩文化',
+    isShow: true,
+    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    createDate: new Date('2025/4/9'),
+    price: 10000,
+  });
 
-  protected isShow = true;
-
-  protected photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
-
-  protected createDate = new Date('2025/4/9');
-
-  protected price = 10000;
+  onSetDisplay(isShow: boolean): void {
+    this.product.isShow = isShow;
+  }
 }
