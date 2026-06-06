4e7e8b81044eb62668599039a3bea6b405d81086
Oliver <ylin73505@gmail.com>
2026-06-06 21:07:39 +0800

    refactor(pagination): 分頁元件變更使用 signal 處理

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output, booleanAttribute, numberAttribute } from '@angular/core';
+import { Component, computed, input, model, numberAttribute, output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,22 +6,15 @@ import { Component, EventEmitter, Input, OnChanges, Output, booleanAttribute, nu
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss',
 })
-export class PaginationComponent implements OnChanges {
-  @Input({ required: true, transform: numberAttribute })
-  totalCount!: number;
+export class PaginationComponent {
+  readonly totalCount = input.required<number, string | number>({ transform: numberAttribute });
 
-  @Input({ required: true, transform: numberAttribute })
-  pageSize!: number;
+  readonly pageSize = input.required<number, string | number>({ transform: numberAttribute });
 
-  @Input({ required: true, transform: numberAttribute })
-  pageIndex!: number;
-  @Output()
-  pageIndexChange = new EventEmitter<number>();
+  readonly pageIndex = model.required<number>();
 
-  range: number[] = [];
-
-  ngOnChanges(): void {
-    const length = Math.ceil(this.totalCount / this.pageSize);
-    this.range = Array.from({ length }, (_, i) => i + 1);
-  }
+  readonly range = computed(() => {
+    const length = Math.ceil(this.totalCount() / this.pageSize());
+    return Array.from({ length }, (_, i) => i + 1);
+  });
 }
