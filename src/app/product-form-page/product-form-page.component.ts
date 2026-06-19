f618eb0b4b2ef0e2128c6ada37e6a850da4117bc
Oliver <ylin73505@gmail.com>
2026-06-19 14:20:33 +0800

    feat(product-form): 產品表單驗證實作

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, input, inject } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { Product } from '../model/product';
 
@@ -15,18 +15,30 @@ export class ProductFormPageComponent {
 
   protected readonly form = new FormGroup({
     id: new FormControl<string | null>(null),
-    name: new FormControl<string | null>(null),
+    name: new FormControl<string | null>(null, { validators: [Validators.required] }),
     authors: new FormArray<FormControl<string | null>>([]),
-    company: new FormControl<string | null>(null),
-    price: new FormControl<number | null>(null),
+    company: new FormControl<string | null>(null, { validators: [Validators.required] }),
+    price: new FormControl<number | null>(null, { validators: [Validators.required] }),
   });
+
+  get name(): FormControl<string | null> {
+    return this.form.get('name') as FormControl<string | null>;
+  }
 
   get authors(): FormArray<FormControl<string | null>> {
     return this.form.get('authors') as FormArray<FormControl<string | null>>;
   }
 
+  get company(): FormControl<string | null> {
+    return this.form.get('company') as FormControl<string | null>;
+  }
+
+  get price(): FormControl<string | null> {
+    return this.form.get('price') as FormControl<string | null>;
+  }
+
   protected onAddAuthor(): void {
-    const formControl = new FormControl<string | null>(null);
+    const formControl = new FormControl<string | null>(null, { validators: [Validators.required] });
     this.authors.push(formControl);
   }
 }
