1c3a8928372c48931faf6e4bb1ec4063571314f5
Oliver <ylin73505@gmail.com>
2026-06-19 14:14:48 +0800

    feat(product-form): 產品表單名稱、出版社、單價欄位設定

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, input, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-form-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './product-form-page.component.html',
   styleUrl: './product-form-page.component.scss',
 })
-export class ProductFormPageComponent {}
+export class ProductFormPageComponent {
+  readonly product = input<Product>();
+
+  protected readonly form = new FormGroup({
+    id: new FormControl<string | null>(null),
+    name: new FormControl<string | null>(null),
+    company: new FormControl<string | null>(null),
+    price: new FormControl<number | null>(null),
+  });
+}
