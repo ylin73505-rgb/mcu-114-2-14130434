848c7caf3ebb895c569ba56413c3469e9d26ca80
Oliver <ylin73505@gmail.com>
2026-06-19 14:17:25 +0800

    feat(product-form): 實作產品作者表單輸入

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, input, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { Product } from '../model/product';
 
@@ -16,7 +16,17 @@ export class ProductFormPageComponent {
   protected readonly form = new FormGroup({
     id: new FormControl<string | null>(null),
     name: new FormControl<string | null>(null),
+    authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null),
     price: new FormControl<number | null>(null),
   });
+
+  get authors(): FormArray<FormControl<string | null>> {
+    return this.form.get('authors') as FormArray<FormControl<string | null>>;
+  }
+
+  protected onAddAuthor(): void {
+    const formControl = new FormControl<string | null>(null);
+    this.authors.push(formControl);
+  }
 }
